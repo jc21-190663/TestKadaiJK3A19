3eff6e95632b0e8a133083a66041df7172f34301
将 佐藤 <190663@jc-21.jp>
2022-02-09 17:45:23 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JetGo
+{
+    public class JetCoaster
+    {
+        public static int ride(int k, int[] g, int r)
+                            //（定員人数、行列、回数
+        {
+            
+            int num = 0;
+            int result = 0;
+
+            //jetに乗る回数分ループ
+            while(r > 0)
+            {
+                int i = 0;
+                //出発のカウント
+                int sv_num = num;
+
+                //定員分ループ、定員k
+                while (i <= k)
+                {
+
+                    //変数iにg[num]番目の要素を加算
+                    i += g[num];
+
+                    //iが定員kより大きかったら
+                    if (i > k)
+                    {
+                        //定員オーバーした変数iから上の処理で加算したg[num]の値を引く
+                        i -= g[num];
+
+                        //resultにiを代入
+                        result += i;
+
+                        //定員オーバーしたのでこの回は終わり
+                        break;
+                    }
+
+                    //店員オーバーしていない場合numを加算
+                    num ++;
+
+                    //numが配列g[]より大きいか？
+                    if (num > g.Length - 1)
+                    {
+                        num = 0;
+                    }
+                    if (sv_num==num)
+                    {
+                        //resultにiを代入
+                        result += i;
+                        break;
+                    }
+                }
+
+                //jetの回数w
+                r --;
+            }
+            return result;
+        }
+    }
+}
---
+++ kuriage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JetGo
+{
+    public class kuriage
+    {
+        public static int kuriage10(int i)
+        {
+            //０
+            if (i == 0)
+            {
+                return 0;
+
+            //プラスの時
+            }else if (i > 0)
+            {
+                double j = i;
+                i = Convert.ToInt32(Math.Ceiling(j / 10) * 10);
+                return i;
+            }
+            //マイナスの時
+            else
+            {
+                double j = i;
+                i = Convert.ToInt32(Math.Floor(j / 10) * 10);
+                return i;
+            }
+            
+        }
+    }
+}
